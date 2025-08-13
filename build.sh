50f1e19c9ad0a79ff8ca1404fc07f051dd8ee6fd
Simon Litt <simon.litt@disroot.org>
2025-08-13 13:45:33 +0300

    Build script

--- build.sh
+++ build.sh
@@ -29,7 +29,6 @@ echo "Version: $version";
 cd ${SCRIPT_DIR}
 if [ $(git tag -l "$version") ]; then
     echo "Tag detected"
-    ${SCRIPT_DIR}/doc_and_minify.sh || die
 
     ver_path="${SCRIPT_DIR}/build/"
 	namespace_var="simonlitt"
@@ -40,6 +39,8 @@ if [ $(git tag -l "$version") ]; then
 
     zip_file_name="${ver_path}jquery-consistent-listbox-$version.zip"
     rm -f "${zip_file_name}"
+    zip_file_name_docs="${ver_path}jquery-consistent-listbox-$version-docs.zip"
+    rm -f "${zip_file_name_docs}"
 
     declare -A publish_file_list
 	publish_file_list['LICENSE']='LICENSE'
@@ -50,13 +51,33 @@ if [ $(git tag -l "$version") ]; then
     for file_name in "${!publish_file_list[@]}"; do
 		file_tag_path=${publish_file_list["$file_name"]}
 		echo "  extract: $file_name..."
-		git show "${version}:$file_tag_path" >> "${build_files}${file_name}" || die
+		git show "${version}:$file_tag_path" > "${build_files}${file_name}" || die
 	done
 
 	echo "Archiving..."
 	cd $TMP_DIR
-	zip -r $zip_file_name "$namespace_var/" || die
+	zip -r ${zip_file_name} "$namespace_var/" || die
 	cd - > /dev/null
+
+	temp_file="${build_files}temp/"
+
+	echo "Extracting '${version}:docs/' files..."
+	mkdir -p "${build_files}docs/" || die
+	mkdir -p  "${temp_file}" || die
+	git ls-tree "${version}:docs/" -r --name-only | while read line ; do echo "  extract: $line..."; git show "${version}:docs/$line" > "${temp_file}_tmp" || die ; install -D "${temp_file}_tmp" "${build_files}docs/$line" ; done
+
+	echo "Extracting '${version}:examples/' files..."
+	mkdir -p "${build_files}examples/" || die
+	mkdir -p  "${temp_file}" || die
+	git ls-tree "${version}:examples/" -r --name-only | while read line ; do echo "  extract: $line..."; git show "${version}:examples/$line" > "${temp_file}_tmp" || die ; install -D "${temp_file}_tmp" "${build_files}examples/$line" ; done
+
+    rm -f -r "${temp_file}"
+
+	echo "Archiving docs version..."
+	cd $TMP_DIR
+	zip -r ${zip_file_name_docs} "$namespace_var/" || die
+	cd - > /dev/null
+
 	echo "Successfully completed!"
 else
     echo "Tag '${version}' not found!"
